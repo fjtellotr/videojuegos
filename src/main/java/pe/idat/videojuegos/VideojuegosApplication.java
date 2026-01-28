3c07f8b738b546e09b741f114f5bf9f40ff2f88e
Junior Tello <fjtello@idat.pe>
2026-01-28 09:22:19 -0500

    Mi primer commit

---
+++ src/main/java/pe/idat/videojuegos/VideojuegosApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.videojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideojuegosApplication.class, args);
+	}
+
+}
